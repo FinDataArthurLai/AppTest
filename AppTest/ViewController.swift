f5a85b22181b63ffad38a9a2510d95b11acf2a9a
Tien Yuan, Wu <tienyuan@findata.com.tw>
2016-05-20 15:24:45 +0800

    Initial Commit

---
+++ AppTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTest
+//
+//  Created by 吳天元 on 5/20/16.
+//  Copyright © 2016 吳天元. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
